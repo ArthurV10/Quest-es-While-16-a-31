42fc4b2c2f0834c0a59b1450ff98529a425b2c6b
Arthur <161647192+ArthurV10@users.noreply.github.com>
2024-03-22 13:19:08 -0300

    Update questão26.js

--- questão26.js
+++ questão26.js
@@ -4,13 +4,18 @@
 //· a média das idades das pessoas que responderam ótimo;
 //· a quantidade de pessoas que respondeu regular;
 //· o percentual de pessoas que respondeu bom entre os entrevistados.
-import {print, pede_numero_positivo} from "../Ajuda/funções.js"
+import {print, pede_numero_positivo, pede_numero} from "../Ajuda/funções.js"
 
 function main(){
     print(`
-    ==============================
+    ====================================
     Analisar Opiniões Sobre Filmes
-    ==============================
+    1 = Otimo
+    2 = Bom
+    3 = Regular
+    4 = Pessimo
+    (Idade '-1' para encerrar o programa)
+    =====================================
     `)
     //Variavel
     let contador_idade_otimo = 0
@@ -19,27 +24,71 @@ function main(){
     let total_pessoas = 0
     let contador_bom = 0
     //Entrada
-    const numero1 = pede_numero_positivo("Digite o primeiro numero: ")
-    const numero2 = pede_numero_positivo("Digite o segundo numero: ")
+    let opinião = verificar_opinião(pede_numero("Digite o numero que corresponde sua opinião: "))
+    let idade = verificar_idade(pede_numero("Digite sua Idade: "))
 
-    //Verificar 
     //Processamento
+    while(idade !== -1){
+        if(opinião === 1){
+            contador_otimo++
+            contador_idade_otimo += idade
+            total_pessoas++
+        }
+        else if(opinião === 2){
+            contador_bom++
+            total_pessoas++
+        }
+        else if(opinião === 3){
+            contador_qtd_pessoas_regular++
+            total_pessoas++
+        }
+        else if(opinião === 4){
+            total_pessoas++
+        }
+        opinião = verificar_opinião(pede_numero("Digite o numero que corresponde sua opinião: "))
+        idade = verificar_idade(pede_numero("Digite sua Idade: "))
+    }
     
+    let media_idade_opinões_otimas = media(contador_idade_otimo, contador_otimo)
+    let percentual_opinão_bom = porcentagem(contador_bom, total_pessoas)
+
     //Saida
     const resposta = `
     ************************************
     ====================================
-    Media de Idade das pessoas que acharam bom:
-    ${media_bom}
+    Media de Idade das pessoas que acharam otimo:
+    ${media_idade_opinões_otimas}
     Quantidade de pessoas que acharam regular:
     ${contador_qtd_pessoas_regular}
     Percentual de pessoas que acharam bom:
-    ${percentual_bom}
+    ${percentual_opinão_bom}%
     ====================================
     ************************************
     `
     print(resposta)
 }
 //Funçôes
-NÃO TA PRONTOOOOOOOOOOOOOOOOOOOO
+function verificar_idade(idade){
+    while(idade < -1 && idade === 0){
+        print("Idade Inexistente")
+        idade = pede_numero("Digite uma idade valida: ")
+    }
+    return idade
+}
+
+function verificar_opinião(opinião){
+    while(opinião !== 1 && opinião !== 2 && opinião !== 3 && opinião !== 4){
+        print("Valor  invalido")
+        opinião = pede_numero("Digite o valor entre as opções apresentadas: ")
+    }
+    return opinião
+}
+
+function media(numero1, numero2) {
+    return numero2 !== 0 ? numero1 / numero2 : 0;
+}
+
+function porcentagem(parcela, total){
+    return (parcela / total) * 100
+}
 main()
